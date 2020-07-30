e54c037421e1cc22b302fb66bd9e34b697d46adc
Sameer Channar <schannar@asu.edu>
2020-07-29 18:51:53 -0700

    more changes

--- game.py
+++ game.py
@@ -30,32 +30,50 @@ def main():
 
 
     count = 0
+    interval = 800
     
-    pygame.display.set_caption("score: " + str(count))
+    pygame.display.set_caption("score: " + str(count) + "        interval: " + str(interval) + "         shapes: " + str(len(shapes)))
    
-    interval = 1000
+
+    shapelimit = 10
+
+    prevTick = 0
+
+    interval = 60
+    clock = pygame.time.Clock()
+    frames = 0
+
 
     while True:
-        if len(shapes) >= 10:
-            pygame.display.set_caption("GAME OVER      score:" + str(count))
-        else:
-            if pygame.time.get_ticks() % interval == 0:
-                if interval > 600:
-                    interval -= 20
+        clock.tick(60)
+        frames += 1
+        
+
+        if len(shapes) >= shapelimit:#if game is over
+            pygame.display.set_caption("GAME OVER      score: " + str(count) + "        interval: " + str(interval) + "         shapes: " + str(len(shapes)))
+        else: #if game is not over
+            if frames % interval == 0: #interval is reached
+                
+                
+                #draw new shape
                 coords.append([random.randrange(0, 800), random.randrange(0, 600)])
                 shapes.append(pygame.draw.circle(gameDisplay, random.choice(colors), coords[len(coords) - 1], 25))
+                pygame.display.set_caption("score: " + str(count) + "        interval: " + str(interval) + "         shapes: " + str(len(shapes)))
+
+        #if game is over or ongoing
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
-            if event.type == pygame.MOUSEBUTTONDOWN and len(shapes) < 10:
+            if event.type == pygame.MOUSEBUTTONDOWN and len(shapes) < shapelimit:
                 for i in range(len(shapes)): 
                     shape = shapes[i]
                     if shape.collidepoint(pygame.mouse.get_pos()):
                         count += 1
-                        pygame.display.set_caption("score: " + str(count))
+                        pygame.display.set_caption("score: " + str(count) + "        interval: " + str(interval) + "         shapes: " + str(len(shapes)))
+                        
 
-                        #cover old shape
+                        #mask old shape with black shape
                         pygame.draw.circle(gameDisplay, black, coords[i], 25)
                         shapes.remove(shape)
                         coords.remove(coords[i])
@@ -63,7 +81,9 @@ def main():
                         
                         break
     
-            
+        if frames % 60 == 0:
+            if interval > 18:
+                interval -= 1
 
         pygame.display.update()
     
